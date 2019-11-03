069c3a367955ab2c8a3297e5f782df74dacf2ff3
builtbybo <lanlecreates@gmail.com>
2019-11-03 19:56:35 +0000

    Updates

---
+++ precache-manifest.8963da2c13c9db3208e600b491f5c393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0311f1519b958cf3c048795f4227a860",
+    "url": "/login-authentication/index.html"
+  },
+  {
+    "revision": "26125212a71f4b165a5d",
+    "url": "/login-authentication/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b72294f3162161eb864e",
+    "url": "/login-authentication/static/js/2.909b818b.chunk.js"
+  },
+  {
+    "revision": "26125212a71f4b165a5d",
+    "url": "/login-authentication/static/js/main.fadca395.chunk.js"
+  },
+  {
+    "revision": "7e11dd71ed51a6a282df",
+    "url": "/login-authentication/static/js/runtime-main.9e16bc03.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/login-authentication/static/media/logo.25bf045c.svg"
+  }
+]);
